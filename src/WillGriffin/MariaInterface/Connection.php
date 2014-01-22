e278d755677133bb06ab046524f1bc045286cc18
Will Griffin <willgriffin@gmail.com>
2014-01-22 06:08:41 -0800

    each and foreach* methods

--- src/WillGriffin/MariaInterface/Connection.php
+++ src/WillGriffin/MariaInterface/Connection.php
@@ -68,6 +68,7 @@ class Connection
   *
   *
   * @param string sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
@@ -92,6 +93,7 @@ class Connection
   *
   *
   * @param string sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
@@ -116,6 +118,7 @@ class Connection
   *
   *
   * @param string sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
@@ -138,11 +141,12 @@ class Connection
   *
   *
   * @param string sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
   *
-  * $fooID = $db->value('select foo_id from bar where bar_id = 1');
+  * $fooID = $db->value('select bar_id from bars where bar_id = 1');
   *
   * ?>
   * </code>
@@ -151,7 +155,6 @@ class Connection
   {
     if ($que = $this->query($sql, $prepArgs))
     {
-
       $row = $que->fetch_row();
       return $row[0];
     } else {
@@ -165,11 +168,12 @@ class Connection
   *
   *
   * @param string sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
   *
-  * $fooIDs = $db->getval('select bar_id from bar where foo_id = 1');
+  * $fooIDs = $db->column('select foo_id from foos');
   *
   * ?>
   * </code>
@@ -188,15 +192,46 @@ class Connection
 
   /**
   *
+  * executes a query and runs a lambda on each results first value
+  *
+  *
+  * @param string $sql sql to execute
+  * @param array $prepArgs optional arguments to send to bind_param for prepared queries
+  * @param object $method function to run on each result
+  * @param object $scope optional scope
+  *
+  * <code>
+  * <?php
+  *  $label = "ID: ";
+  *  $db->foreachValue(
+  *    "select id from foobar where id > ?", ['i',1],
+  *    function($result, $scope) {
+  *      echo $scope.$result."\n";
+  *    },
+  *    $label);
+  * ?>
+  * </code>
+  */
+
+  public function foreachValue()
+  {
+    $args = func_get_args();
+    return $this->each('value', $args[0], $args[1], $args[2], $args[3]);
+  }
+
+
+  /**
+  *
   * returns result of query as json
   *
   *
   * @param mixed key sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
   *
-  *
+  * $jsonTxt = $db->json("select * from foos");
   *
   * ?>
   * </code>
@@ -212,11 +247,15 @@ class Connection
   *
   *
   * @param string $sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
   *
+  * $fooArr = $db->assoc("select * from foos");
   *
+  * //prepared
+  * $foo = $db->assoc("select * from foos where foo_id > ?", ['i',0]);
   *
   * ?>
   * </code>
@@ -247,11 +286,15 @@ class Connection
   *
   *
   * @param string $sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
   *
+  * $foo = $db->assoc("select * from foos where foo_id = 1");
   *
+  * //prepared
+  * $foo = $db->assoc("select * from foos where foo_id = ?", ['i',1]);
   *
   * ?>
   * </code>
@@ -267,7 +310,34 @@ class Connection
     }
   }
 
+  /**
+  *
+  * executes a query and runs a lambda on each resulting row passed as an associative array
+  *
+  *
+  * @param string $sql sql to execute
+  * @param array $prepArgs optional arguments to send to bind_param for prepared queries
+  * @param object $method function to run on each result
+  * @param object $scope optional scope
+  *
+  * <code>
+  * <?php
+  *  $obj = ["foo" => "ID: "];
+  *  $db->foreachAssoc(
+  *    "select id from foobar where id > ?", ['i',1],
+  *    function($result, $scope) {
+  *      echo $scope['foo'].$result['id']."\n";
+  *    },
+  *    $obj);
+  * ?>
+  * </code>
+  */
 
+  public function foreachAssoc()
+  {
+    $args = func_get_args();
+    return $this->each('assoc', $args[0], $args[1], $args[2], $args[3]);
+  }
 
 
   /**
@@ -276,10 +346,15 @@ class Connection
   *
   *
   * @param string $sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
   *
+  * $foo = $db->object("select * from foos where foo_id = 1");
+  *
+  * //prepared
+  * $foo = $db->object("select * from foos where foo_id = ?", ['i',1]);
   *
   *
   * ?>
@@ -295,17 +370,23 @@ class Connection
     }
   }
 
+
+
   /**
   *
   * returns result of query as array of objects
   *
   *
   * @param string $sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
   *
+  * $foos = $db->objects("select * from foos");
   *
+  * //prepared
+  * $foos = $db->objects("select * from foos where foo_id > 0", ['i',0]);
   *
   * ?>
   * </code>
@@ -324,10 +405,132 @@ class Connection
 
   /**
   *
+  * executes a query and runs a lambda on each resulting row passed as an object
+  *
+  *
+  * @param string $sql sql to execute
+  * @param array $prepArgs optional arguments to send to bind_param for prepared queries
+  * @param object $method function to run on each result
+  * @param object $scope optional scope
+  *
+  * <code>
+  * <?php
+  *  $obj = (object)["foo" => "ID: "];
+  *  $db->foreachObject(
+  *    "select id from foobar where id > ?", ['i',1],
+  *    function($result, $scope) {
+  *      echo $scope->foo.$result->id."\n";
+  *    },
+  *    $obj);
+  * ?>
+  * </code>
+  */
+
+  public function foreachObject()
+  {
+    $args = func_get_args();
+    return $this->each('object', $args[0], $args[1], $args[2], $args[3]);
+  }
+
+  /**
+  *
+  * executes a query and runs a lambda on each resulting row passed as $type
+  *
+  *
+  * @param string $type array/object/assoc/row/value
+  * @param string $sql sql to execute
+  * @param array $prepArgs bind_param arguments for prepared query
+  * @param function $method function to run on each result
+  * @param function $scope optional scope
+  *
+  * or
+  *
+  * @param string $type object/assoc/row
+  * @param string $sql sql to execute
+  * @param function $method function to run on each result
+  * @param function $scope optional scope
+  *
+  *
+  * <code>
+  * <?php
+  *
+  * $db->each('select * from foo', function($result) {
+  *   $row = $result->fetch_row();
+  *   var_dump($row);
+  * });
+  *
+  * //basic
+  * $db->each(
+  *   'object',
+  *   "select id from foobar where id > 0",
+  *   function($result) {
+  *     echo $result->id."\n";
+  *   });
+  *
+  * //prepared
+  * $db->each(
+  *   'assoc',
+  *   "select id from foobar where id = ? or id = ?",
+  *   ['ii',1,2],
+  *   function($result) {
+  *     echo $result['id']."\n";
+  * });
+  *
+  * //prepared with scope
+  *
+  * $db->each(
+  *   'object',
+  *   "select * from foobar where id > ?",
+  *   ['ii',0],
+  *   function($result, $scope) {
+  *     echo $scope->foo.$result[0]."\n";
+  *   },
+  *   $this);
+  *
+  * ?>
+  * </code>
+  */
+  public function each()
+  {
+
+    $args = func_get_args();
+    if (gettype($args[2]) == 'array') {
+      $result = $this->query($args[1], $this->mkrefs($args[2]));
+      list($method,$scope) = [3,4];
+    } else if (gettype($args[2]) == 'object') {
+      $result = $this->query($args[1]);
+      list($method,$scope) = [2,3];
+    }
+
+    if ($args[0] != 'value') {
+      $resultMethod = new \ReflectionMethod('mysqli_result', "fetch_".$args[0]);
+      while ($row = $resultMethod->invoke($result)) {
+        $args[$method]($row, $args[$scope]);
+      }
+    } else {
+      $resultMethod = new \ReflectionMethod('mysqli_result', "fetch_row");
+      while ($row = $resultMethod->invoke($result)) {
+        $args[$method]($row[0], $args[$scope]);
+      }
+    }
+  }
+
+  //ugh
+  //todo: find a better way
+  public function mkrefs($arr) {
+    $refs = array();
+    foreach($arr as $key => $value)
+      $refs[$key] = &$arr[$key];
+    return $refs;
+  }
+
+  /**
+  *
   * executes query returns result
   *
   *
   * @param string $sql sql to execute
+  * @param array $prepArgs arguments to send to bind_param for prepared queries
   *
   * <code>
   * <?php
@@ -352,7 +555,7 @@ class Connection
           $this->error("Couldn't prepare statement");
         } else {
           $method = new \ReflectionMethod('mysqli_stmt', 'bind_param');
-          $method->invokeArgs($stmt,$prepArgs); /* much love to jan kriedner */
+          $method->invokeArgs($stmt, $prepArgs); /* much love to jan kriedner */
           $stmt->execute();
           $result = $stmt->get_result();
         }
@@ -412,8 +615,8 @@ class Connection
   }
 
   /**
-  *
-  * loads structure of database. from opcache if initialized.
+  * probably broken
+  * loads structure of database. from opcache if initialized. (broken)
   *
   *
   * <code>
@@ -426,11 +629,9 @@ class Connection
   */
   function loadStructure()
   {
-    //global $phijit;
 
     $startTime = microtime(true);
     $cached = $this->cache->dbstruct;
-    //$cached = $_SESSION['dbstruct'];
     if (empty($cached))
     {
       $dbtables = $this->getlist("show tables");
@@ -495,7 +696,7 @@ class Connection
 
 
   /**
-  *
+  * probably broken
   * check to see that two records are related
   *
   * @param string $descendant_table descendant table
@@ -544,7 +745,7 @@ class Connection
   }
 
   /**
-  *
+  * probably broken
   * try and find how two tables are related. find the salemen at least one path, could get weird
   *
   *
